daedd799539f5b9d5be177cca39be826a7541bad
Yash Sharma <yashsharma228>
2026-02-21 15:24:08 +0530

    Saved my files changes

--- website/src/lib/firebase.js
+++ website/src/lib/firebase.js
@@ -1,5 +1,7 @@
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,5 +12,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
