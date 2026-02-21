9c3d690c95581068740e3abbe66ed46c4e6bfb46
Yash Sharma <yashsharma228>
2026-02-21 18:10:21 +0530

    chore: update CSP in website next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -1,23 +1,54 @@
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' https://apis.google.com https://www.gstatic.com;
-  connect-src 'self' https://image-gallery-app-9x2r.onrender.com https://*.firebaseio.com https://identitytoolkit.googleapis.com;
-  img-src 'self' data: https://res.cloudinary.com;
-  style-src 'self' 'unsafe-inline';
-  font-src 'self' data:;
+
+  script-src 
+    'self' 
+    'unsafe-inline' 
+    https://apis.google.com 
+    https://www.gstatic.com 
+    https://accounts.google.com;
+
+  connect-src 
+    'self' 
+    https://image-gallery-app-9x2r.onrender.com
+    https://identitytoolkit.googleapis.com
+    https://securetoken.googleapis.com
+    https://www.googleapis.com
+    https://*.firebaseio.com
+    https://firestore.googleapis.com;
+
+  img-src 
+    'self' 
+    data: 
+    https://res.cloudinary.com 
+    https://lh3.googleusercontent.com;
+
+  style-src 
+    'self' 
+    'unsafe-inline';
+
+  font-src 
+    'self' 
+    data:;
+
+  frame-src 
+    https://accounts.google.com;
 `;
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: '*.amazonaws.com',
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
@@ -28,10 +59,10 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
+            key: "Content-Security-Policy",
             value: ContentSecurityPolicy.replace(/\n/g, ""),
           },
         ],
