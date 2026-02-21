d9543481cc9b7569a3f876bd420ee63db441a55f
Yash Sharma <yashsharma228>
2026-02-21 17:59:15 +0530

    chore: update CSP in website next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -1,3 +1,12 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' https://apis.google.com https://www.gstatic.com;
+  connect-src 'self' https://image-gallery-app-9x2r.onrender.com https://*.firebaseio.com https://identitytoolkit.googleapis.com;
+  img-src 'self' data: https://res.cloudinary.com;
+  style-src 'self' 'unsafe-inline';
+  font-src 'self' data:;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,7 +21,7 @@ const nextConfig = {
       },
     ],
   },
-}
+};
 
 module.exports = {
   ...nextConfig,
@@ -23,7 +32,7 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; font-src 'self' data:; img-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
           },
         ],
       },
