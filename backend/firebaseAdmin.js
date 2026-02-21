226fc4c8d7606f76a6a9b35585c1f2c019d6b164
Yash Sharma <yashsharma228>
2026-02-21 16:33:11 +0530

    fix: use env variables for Firebase Admin initialization

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,12 @@
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = admin;
