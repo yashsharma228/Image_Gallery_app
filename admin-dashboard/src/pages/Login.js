80cd8236998a86cad14b6938dfde514a13480e46
Yash Sharma <yashsharma228>
2026-04-21 17:35:17 +0530

    saved login changes

--- admin-dashboard/src/pages/Login.js
+++ admin-dashboard/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authAPI } from '../api';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../lib/firebase';
 
 const Login = () => {
